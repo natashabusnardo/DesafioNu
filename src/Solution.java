00eb4c690957c670814d056b7304807891974a5a
natashabusnardo <natashabusnardo@hotmail.com>
2022-02-19 13:43:38 -0300

    Solucao otimizada

--- src/Solution.java
+++ src/Solution.java
@@ -4,33 +4,29 @@ import java.util.*;
 class Result {
     public static int acharMinimoDeDias(List<Float> duracoes) {
         int dias = 0;
-        do{
-            for(int i = 0; i < duracoes.size(); i++){
-                if(duracoes.get(i) > 1.99 | duracoes.size() == 1){
-                    dias++;
-                    duracoes.remove(i);
-                    break;
-                }
-                else{
-                    for (int j = 1; j < duracoes.size(); j++){
-                        if(duracoes.get(i) + duracoes.get(j) <= 3.0){
-                            duracoes.remove(i);
-                            duracoes.remove(j-1);
-                            break;
-                        }
-                    }
-                    dias++;
-                }
-                break;
-            }
-        }while (duracoes.size() != 0);
+        Collections.sort(duracoes);
+        System.out.println(duracoes);
 
+        while (duracoes.size() != 0) {
+            System.out.println(duracoes);
+            if (duracoes.size() == 1) {
+                dias++;
+                duracoes.remove(0);
+            } else if (duracoes.get(0) + duracoes.get((duracoes.size() - 1)) <= 3.0) {
+                dias++;
+                duracoes.remove(0);
+                duracoes.remove((duracoes.size() - 1));
+            } else {
+                duracoes.remove((duracoes.size() - 1));
+                dias++;
+            }
+        }
         return dias;
     }
 }
 
 public class Solution {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
         int duracoesCount = sc.nextInt();
